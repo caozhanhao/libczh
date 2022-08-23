c1f329bbcacec44bb1b949179fbd472664308aa1
caozhanhao <cao2013zh@163.com>
2022-08-23 10:45:54 +0800

    update Copyright

--- src/lexer.hpp
+++ src/lexer.hpp
@@ -1,4 +1,4 @@
-﻿//   Copyright 2022 caozhanhao
+﻿//   Copyright 2021-2022 libczh - caozhanhao
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
